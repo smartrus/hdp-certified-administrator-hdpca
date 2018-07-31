3e5bf9c335b828a2d0486e32f90499705e38f3d8
Rustem Abdrakhmanov <rustemabd@gmail.com>
2018-07-31 11:57:53 +0300

    setup fixed

--- setup_aws_redhat.sh
+++ setup_aws_redhat.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-pscp.pssh -v -l centos -h cluster-hosts.txt -x " -oStrictHostKeyChecking=no" 'chmod u+x /home/centos/prepareNode.sh && /home/centos/prepareNode.sh >> prepareNode.log && sudo reboot'
+pscp.pssh -v -l centos -h cluster_hosts.txt -x " -oStrictHostKeyChecking=no" prepareNode.sh /home/centos/prepareNode.sh
+
+pssh -v -t 0 -l centos -h cluster_hosts.txt -x "-t -t -oStrictHostKeyChecking=no" 'chmod u+x /home/centos/prepareNode.sh && /home/centos/prepareNode.sh >> prepareNode.log && sudo reboot'
