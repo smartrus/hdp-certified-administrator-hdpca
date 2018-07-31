77000f490a235c92a8e28dc0cbcf31a519fc0c58
Rustem Abdrakhmanov <rustemabd@gmail.com>
2018-07-31 12:15:31 +0300

    prepare node fixed

--- prepareNode.sh
+++ prepareNode.sh
@@ -4,15 +4,14 @@ echo "umask 022" ? ~/.bashrc
 
 sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 
-sudo chkconfig ntpd on
-sudo ntpd -q
-sudo service ntpd start
+#sudo chkconfig ntpd on
+#sudo ntpd -q
+#sudo service ntpd start
 
-sudo service iptables stop
-sudo chkconfig iptables off
-sudo service ip6tables stop
-sudo chkconfig ip6tables off
-
+#sudo service iptables stop
+#sudo chkconfig iptables off
+#sudo service ip6tables stop
+#sudo chkconfig ip6tables off
 echo 0 | sudo tee /proc/sys/vm/swappiness
 echo vm.swappiness = 0 | sudo tee -a /etc/sysctl.conf
 
